3ebf89c8bae2018abe52feb26e2744ce992b0a96
Sahand Shokouhi <sahand.shok@gmail.com>
2018-01-13 04:38:19 +0330

    fix bug of not loading css contents

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res, next) => {
-  return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res, next) => {
+  return res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(8080, function () {
